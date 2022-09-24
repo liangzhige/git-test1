a6757be828f47b3df365491b12ef9322988ede98
liangzhige <114218726+liangzhige@users.noreply.github.com>
2022-09-24 17:07:40 +0800

    pull commit

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -8,5 +8,7 @@ public class GitTest {
         System.out.println("master test!!");
         System.out.println("hot-fix test!!");
         System.out.println("push test!!");
+        System.out.println("小小雷牛，不怕困难!!");
+        System.out.println("嘿嘿嘿!!");
     }
 }
